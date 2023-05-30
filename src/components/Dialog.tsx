80c719943f43c261e0e92e5214e76695a3683dac
tespin <tristan.m.espinoza@gmail.com>
2023-05-30 14:01:40 -0700

    fix: index button and dialog popup shows full content

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { ChevronUpIcon, ChevronDownIcon } from '@radix-ui/react-icons';
+import FlexContainer from './FlexContainer';
+
+interface DialogProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const Dialog = ({ className, children }: DialogProps) => {
+  // const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <DialogPrimitive.Root>
+      <DialogPrimitive.Trigger asChild>
+        <button className='flex flex-col items-center px-6 py-2 border border-black'>
+          <ChevronUpIcon />
+          <p>Index</p>
+        </button>
+      </DialogPrimitive.Trigger>
+      <DialogPrimitive.Portal className='xs:w-full'>
+        <DialogPrimitive.Overlay className='xs:fixed xs:inset-0 backdrop-blur-sm xs:bg-black/20' />
+        <DialogPrimitive.Content className='xs:flex xs:flex-col items-center xs:w-full absolute bottom-0 left-2/4 -translate-x-2/4'>
+          {children}
+          <DialogPrimitive.Close asChild>
+            <button className='flex flex-col items-center absolute bottom-4 px-6 py-2 bg-black text-white '>
+              <ChevronDownIcon />
+              <p>Index</p>
+            </button>
+          </DialogPrimitive.Close>
+        </DialogPrimitive.Content>
+      </DialogPrimitive.Portal>
+    </DialogPrimitive.Root>
+  );
+};
+
+export default Dialog;
