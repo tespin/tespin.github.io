e82cdb7bcecbf1b8c54512920197d57b762464ab
tespin <tristan.m.espinoza@gmail.com>
2023-05-31 13:29:05 -0700

    feat: added project list to homepage

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -14,7 +14,7 @@ const Dialog = ({ className, children }: DialogProps) => {
   return (
     <DialogPrimitive.Root>
       <DialogPrimitive.Trigger asChild>
-        <button className='flex flex-col items-center px-6 py-2 border border-black'>
+        <button className='flex flex-col px-6 py-2 items-center border border-black hover:drop-shadow-lg '>
           <ChevronUpIcon />
           <p>Index</p>
         </button>
@@ -24,7 +24,7 @@ const Dialog = ({ className, children }: DialogProps) => {
         <DialogPrimitive.Content className='xs:flex xs:flex-col items-center xs:w-full absolute bottom-0 left-2/4 -translate-x-2/4'>
           {children}
           <DialogPrimitive.Close asChild>
-            <button className='flex flex-col items-center absolute bottom-4 px-6 py-2 bg-black text-white '>
+            <button className='flex flex-col items-center absolute bottom-4 px-6 py-2 bg-black text-white border-transparent'>
               <ChevronDownIcon />
               <p>Index</p>
             </button>
