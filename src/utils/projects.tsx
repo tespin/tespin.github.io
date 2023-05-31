c1a46b5fc513163221f4d170d312e5b13ecff931
tespin <tristan.m.espinoza@gmail.com>
2023-05-31 13:28:44 -0700

    feat: added utils folder, contains array of projects

---
+++ src/utils/projects.tsx
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    title: 'Owen Wilson Search Engine',
+    date: 'May 2023',
+    brief: "Search for every movie where Owen Wilson says 'Wow'.",
+  },
+  {
+    title: 'AnyInfo',
+    date: 'February 2023',
+    brief: 'Summarize corporate privacy policies with AI.',
+  },
+  {
+    title: 'Banned Prison Books API',
+    date: 'January 2023',
+    brief: 'A free Rest API for books banned in state prisons.',
+  },
+  {
+    title: 'Cloud View',
+    date: 'November 2022',
+    brief: 'Collect images of the sky above you, wherever you are.',
+  },
+];
