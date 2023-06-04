87bb16ed71efd10ddc5f2cc36233920741003f46
tespin <tristan.m.espinoza@gmail.com>
2023-06-04 10:29:58 -0700

    fix: render portal on client

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -17,35 +17,33 @@ const Dialog = ({ children }: DialogProps) => {
   });
 
   return (
-    <>
+    <DialogPrimitive.Root open={open} onOpenChange={setOpen}>
+      <DialogPrimitive.Trigger asChild>
+        <button className='flex flex-col px-6 py-4 mb-4 items-center border border-black group'>
+          <ChevronUpIcon className='transition duration-100 group-hover:translate-y-[-2px] sm:group-hover:animate-chevronUp' />
+          <p>Index</p>
+        </button>
+      </DialogPrimitive.Trigger>
       {isClient ? (
-        <DialogPrimitive.Root open={open} onOpenChange={setOpen}>
-          <DialogPrimitive.Trigger asChild>
-            <button className='flex flex-col px-6 py-4 mb-4 items-center border border-black group'>
-              <ChevronUpIcon className='transition duration-100 group-hover:translate-y-[-2px] sm:group-hover:animate-chevronUp' />
-              <p>Index</p>
-            </button>
-          </DialogPrimitive.Trigger>
-          <DialogPrimitive.Portal className='xs:w-full xs:flex xs:flex-col xs:relative xs:justify-center'>
-            <DialogPrimitive.Overlay className='xs:fixed xs:inset-0 backdrop-blur-md xs:bg-base/20' />
-            <DialogPrimitive.Content
-              className={`xs:flex xs:flex-col items-center xs:w-full h-screen pt-4 pb-[120px] absolute bottom-0 overflow-y-scroll ${
-                open ? 'animate-listShow' : 'animate-listHide'
-              } `}
-            >
-              {children}
-              <div className='fixed bg-base w-full h-[96px] left-0 bottom-0 shadow-top '></div>
-              <DialogPrimitive.Close asChild>
-                <button className='xs:flex xs:flex-col items-center fixed bottom-8 px-6 py-4 bg-black text-white border border-black group'>
-                  <ChevronDownIcon className='transition duration-100 translate-y-[-2px] group-hover:translate-y-[0px] sm:group-hover:animate-chevronDown' />
-                  <p>Index</p>
-                </button>
-              </DialogPrimitive.Close>
-            </DialogPrimitive.Content>
-          </DialogPrimitive.Portal>
-        </DialogPrimitive.Root>
+        <DialogPrimitive.Portal className='xs:w-full xs:flex xs:flex-col xs:relative xs:justify-center'>
+          <DialogPrimitive.Overlay className='xs:fixed xs:inset-0 backdrop-blur-md xs:bg-base/20' />
+          <DialogPrimitive.Content
+            className={`xs:flex xs:flex-col items-center xs:w-full h-screen pt-4 pb-[120px] absolute bottom-0 overflow-y-scroll ${
+              open ? 'animate-listShow' : 'animate-listHide'
+            } `}
+          >
+            {children}
+            <div className='fixed bg-base w-full h-[96px] left-0 bottom-0 shadow-top '></div>
+            <DialogPrimitive.Close asChild>
+              <button className='xs:flex xs:flex-col items-center fixed bottom-8 px-6 py-4 bg-black text-white border border-black group'>
+                <ChevronDownIcon className='transition duration-100 translate-y-[-2px] group-hover:translate-y-[0px] sm:group-hover:animate-chevronDown' />
+                <p>Index</p>
+              </button>
+            </DialogPrimitive.Close>
+          </DialogPrimitive.Content>
+        </DialogPrimitive.Portal>
       ) : null}
-    </>
+    </DialogPrimitive.Root>
   );
 };
 
